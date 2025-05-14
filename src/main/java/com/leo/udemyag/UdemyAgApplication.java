a42af0b1c8ee10bac907beb6cc27cb6f46c83088
leomencho <leobollini@gmail.com>
2025-05-14 00:15:41 -0300

    app simple con rest y controller.

---
+++ src/main/java/com/leo/udemyag/UdemyAgApplication.java
@@ -0,0 +1,13 @@
+package com.leo.udemyag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyAgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyAgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leo/udemyag/UdemyAgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.udemyag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyAgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
